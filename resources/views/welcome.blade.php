fc71108ebfb93cd2f97f61eec2d4636b430e80de
a-b63 <it@a-b63.ru>
2021-09-21 11:39:36 +0400

    done title screen adaptive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,69 +15,77 @@
         <main class="main-index">
             <h1 class="visually-hidden">Список игр на пенсию</h1>
             <div class="castle">
-            <div class="castle__logo">
-                <picture>
-                    <source media="(max-width: 1280px)" srcset="/images/logo-1280.png">
-                    <source media="(max-width: 1600px)" srcset="/images/logo-1600.png">
-                    <img src="/images/logo.png" width="960" height="50" alt="Список игр на пенсию" class="castle__logo-img">
-                </picture>
+                <div class="castle__logo">
+                    <picture>
+                        <source media="(max-width: 575px)" srcset="/images/logo-576.png">
+                        <source media="(max-width: 767px)" srcset="/images/logo-768.png">
+                        <source media="(max-width: 1279px)" srcset="/images/logo-1280.png">
+                        <source media="(max-width: 1599px)" srcset="/images/logo-1600.png">
+                        <img src="/images/logo.png" width="960" height="50" alt="Список игр на пенсию" class="castle__logo-img">
+                    </picture>
+                </div>
+
+                <div class="castle__auth">
+                    <form class="castle__form auth-form" action="/">
+                    <div class="auth-form__item">
+                        <label for="ligin">Логин или e-mail</label>
+                        <input class="auth-form__input" type="text" id="login" name="login" placeholder="Логин или e-mail">
+                    </div>
+                    <div class="auth-form__item">
+                        <label for="password">Пароль</label>
+                        <input class="auth-form__input" type="password" id="password" name="password" placeholder="Пароль">
+                    </div>
+                    <div class="auth-form__buttons">
+                        <button class="auth-form__sign-in" type="submit" formaction="/">
+                        Войти
+                        </button>
+                        <button class="auth-form__reg" type="submit" formaction="/">
+                        Зарегистрироваться
+                        </button>
+                    </div>
+                    <div class="auth-form__guest auth-form__guest--forget">
+                        <a class="auth-form__link" href="#">Я забыл пароль</a>
+                    </div>
+                    <div class="auth-form__guest">
+                        <a class="auth-form__link" href="gamelist.html">Войти как гость</a>
+                    </div>
+                    </form>
+                </div>
+
+                <div class="bird bird-big bird-one bird-normal"></div>
+                <div class="bird bird-big bird-one bird-up"></div>
+                <div class="bird bird-big bird-two bird-up"></div>
+                <div class="bird bird-big bird-two bird-normal"></div>
+                <div class="bird bird-small bird-three bird-up"></div>
+                <div class="bird bird-small bird-three bird-normal"></div>
+                <div class="bird bird-small bird-four bird-normal"></div>
+                <div class="bird bird-small bird-four bird-up"></div>
+                <div class="bird bird-small bird-five bird-up"></div>
+                <div class="bird bird-small bird-five bird-normal"></div>
+                <div class="bird bird-small bird-six bird-normal"></div>
+                <div class="bird bird-small bird-six bird-up"></div>
+
+                <div class="flag flag-left flag-normal"></div>
+                <div class="flag flag-left flag-up"></div>
+                <div class="flag flag-right flag-normal"></div>
+                <div class="flag flag-right flag-up"></div>
+
+                <button class="castle__button" type="button">
+                    press start
+                </button>
+
+                <div class="sumo sumo-stand"></div>
+                <div class="sumo sumo-walk-1"></div>
+                <div class="sumo sumo-walk-2"></div>
+
+                <div class="link"></div>
+
+                <div class="castle__copyright">
+                    © 2021 rgamelist.rg - Уютный проект, который поможет вести учет Ваших игр
+                </div>
             </div>
 
-            <div class="castle__auth">
-                <form class="castle__form auth-form" action="/">
-                <div class="auth-form__item">
-                    <label for="ligin">Логин или e-mail</label>
-                    <input class="auth-form__input" type="text" id="login" name="login" placeholder="Логин или e-mail">
-                </div>
-                <div class="auth-form__item">
-                    <label for="password">Пароль</label>
-                    <input class="auth-form__input" type="password" id="password" name="password" placeholder="Пароль">
-                </div>
-                <div class="auth-form__buttons">
-                    <button class="auth-form__sign-in" type="submit" formaction="/">
-                    Войти
-                    </button>
-                    <button class="auth-form__reg" type="submit" formaction="/">
-                    Зарегистрироваться
-                    </button>
-                </div>
-                <div class="auth-form__guest">
-                    <a class="auth-form__link" href="#">Я забыл пароль</a>
-                </div>
-                <div class="auth-form__guest">
-                    <a class="auth-form__link" href="gamelist.html">Войти как гость</a>
-                </div>
-                </form>
-            </div>
 
-            <div class="bird bird-big bird-one bird-normal"></div>
-            <div class="bird bird-big bird-one bird-up"></div>
-            <div class="bird bird-big bird-two bird-up"></div>
-            <div class="bird bird-big bird-two bird-normal"></div>
-            <div class="bird bird-small bird-three bird-up"></div>
-            <div class="bird bird-small bird-three bird-normal"></div>
-            <div class="bird bird-small bird-four bird-normal"></div>
-            <div class="bird bird-small bird-four bird-up"></div>
-            <div class="bird bird-small bird-five bird-up"></div>
-            <div class="bird bird-small bird-five bird-normal"></div>
-            <div class="bird bird-small bird-six bird-normal"></div>
-            <div class="bird bird-small bird-six bird-up"></div>
-
-            <div class="flag flag-left flag-normal"></div>
-            <div class="flag flag-left flag-up"></div>
-            <div class="flag flag-right flag-normal"></div>
-            <div class="flag flag-right flag-up"></div>
-
-            <button class="castle__button" type="button">
-                press start
-            </button>
-
-            <div class="sumo sumo-stand"></div>
-            <div class="sumo sumo-walk-1"></div>
-            <div class="sumo sumo-walk-2"></div>
-
-            <div class="link"></div>
-            </div>
         </main>
         {{-- <script src="/bundle.js"></script> --}}
     </body>
